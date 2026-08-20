cb5e69015d8cd70625b4338af1366089e697ec25
ibaral-25 <ibaral@fusd.net>
2026-08-20 00:58:36 +0000

    Ibarra.Lab5

---
+++ ibarraL5.py
@@ -0,0 +1,33 @@
+#Alejandra Ibarra
+#Lab 5
+
+evens = [2,4,6,8,10,12,14,16,18,20]
+
+print(evens[0])
+
+print(evens[1])
+
+print(evens[2])
+
+print(evens[3])
+
+print(evens[4])
+
+print(evens[5])
+
+print(evens[6])
+
+print(evens[7])
+
+print(evens[8])
+
+print(evens[9])
+
+for i in evens:
+    print(i)
+
+fruits = ["apples", "mangos", "pears", "bananas", "peaches"]
+
+for j in fruits:
+    print(j)
+
